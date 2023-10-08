f9a2c850a35446cc7b5c0ccdc05f0128a3f39408
Arjun S <arjun8107@gmail.com>
2023-10-09 01:13:26 +0530

    uploadjson route

--- main.ts
+++ main.ts
@@ -7,7 +7,12 @@ const kv = await Deno.openKv();
 router.use(async (context, next) => {
   const start = Date.now();
   await next();
-  console.log(context.request.method, context.request.url.pathname, Date.now() - start, "ms");
+  console.log(
+    context.request.method,
+    context.request.url.pathname,
+    Date.now() - start,
+    "ms"
+  );
 });
 
 router
@@ -17,7 +22,7 @@ router
   .post("/quiz", async (context) => {
     const body = context.request.body();
     if (body.type === "json") {
-      const value:{
+      const value: {
         name: string;
         email: string;
       } = await body.value;
@@ -29,45 +34,54 @@ router
           context.response.status = 400;
           return;
         }
-        const found = await kv.get(["quiz",value.email]);
+        const found = await kv.get(["quiz", value.email]);
         if (!found.value) {
-          await kv.set(["quiz",value.email], value.name);
+          await kv.set(["quiz", value.email], value.name);
           context.response.body = "user added.";
         } else {
           context.response.body = "user already exists";
           context.response.status = 400;
         }
       } else {
-        context.response.body = "Invalid user. data must be {name: string, email: string}";
+        context.response.body =
+          "Invalid user. data must be {name: string, email: string}";
         context.response.status = 400;
       }
     } else {
-      context.response.body = "Invalid request. data must be json. Make sure to set Content-Type header to application/json";
+      context.response.body =
+        "Invalid request. data must be json. Make sure to set Content-Type header to application/json";
       context.response.status = 400;
     }
   })
   .get("/quiz/render", async (context) => {
-    const list = kv.list({prefix:["quiz"]});
+    const list = kv.list({ prefix: ["quiz"] });
     const users = [];
-    for await (const {key, value} of list) {
-      users.push({email: key[1], name: value});
+    for await (const { key, value } of list) {
+      users.push({ email: key[1], name: value });
     }
     // import html file from templates/quiz.html
     const html = await Deno.readTextFile("./templates/quiz.html");
     // replace {{users}} with users
-    context.response.body = html.replace("{{users}}", `
-      ${users.map(user => `
+    context.response.body = html.replace(
+      "{{users}}",
+      `
+      ${users
+        .map(
+          (user) => `
       <tr>
         <td>${user.name}</td>
         <td>${user.email}</td>
       </tr>
-      `).join("")}
-    `);
+      `
+        )
+        .join("")}
+    `
+    );
   })
   .get("/quiz/:email", async (context) => {
     const email = context.params.email;
     if (email) {
-      const found = await kv.get(["quiz",email]);
+      const found = await kv.get(["quiz", email]);
       if (found.value) {
         context.response.body = found.value;
       } else {
@@ -82,9 +96,9 @@ router
   .delete("/quiz/:email", async (context) => {
     const email = context.params.email;
     if (email) {
-      const found = await kv.get(["quiz",email]);
+      const found = await kv.get(["quiz", email]);
       if (found.value) {
-        await kv.delete(["quiz",email]);
+        await kv.delete(["quiz", email]);
         context.response.body = "user deleted";
       } else {
         context.response.body = "user not found";
@@ -96,17 +110,17 @@ router
     }
   })
   .get("/quiz", async (context) => {
-    const list = kv.list({prefix:["quiz"]});
+    const list = kv.list({ prefix: ["quiz"] });
     const users = [];
-    for await (const {key, value} of list) {
-      users.push({email: key[1], name: value});
-    context.response.body = users;
+    for await (const { key, value } of list) {
+      users.push({ email: key[1], name: value });
+      context.response.body = users;
     }
   })
   .post("/goal", async (context) => {
     const body = context.request.body();
     if (body.type === "json") {
-      const value:{
+      const value: {
         name: string;
         institution: string;
         location: string;
@@ -120,9 +134,13 @@ router
           context.response.status = 400;
           return;
         }
-        const found = await kv.get(["goal",value.email]);
+        const found = await kv.get(["goal", value.email]);
         if (!found.value) {
-          await kv.set(["goal",value.email], {name: value.name, institution: value.institution, location: value.location});
+          await kv.set(["goal", value.email], {
+            name: value.name,
+            institution: value.institution,
+            location: value.location,
+          });
           context.response.body = "user added.";
         } else {
           context.response.body = "user already exists";
@@ -130,34 +148,51 @@ router
         }
       }
     } else {
-      context.response.body = "Invalid request. data must be json. Make sure to set Content-Type header to application/json";
+      context.response.body =
+        "Invalid request. data must be json. Make sure to set Content-Type header to application/json";
       context.response.status = 400;
     }
   })
   .get("/goal/render", async (context) => {
-    const list = kv.list<{name: string; institution: string; location: string;}>({prefix:["goal"]});
+    const list = kv.list<{
+      name: string;
+      institution: string;
+      location: string;
+    }>({ prefix: ["goal"] });
     const users = [];
-    for await (const {key, value} of list) {
-      users.push({email: key[1], name: value.name, institution: value.institution, location: value.location});
+    for await (const { key, value } of list) {
+      users.push({
+        email: key[1],
+        name: value.name,
+        institution: value.institution,
+        location: value.location,
+      });
     }
     // import html file from templates/goal.html
     const html = await Deno.readTextFile("./templates/goal.html");
     // replace {{users}} with users
-    context.response.body = html.replace("{{users}}", `
-      ${users.map(user => `
+    context.response.body = html.replace(
+      "{{users}}",
+      `
+      ${users
+        .map(
+          (user) => `
       <tr>
         <td>${user.name}</td>
         <td>${user.institution}</td>
         <td>${user.location}</td>
         <td>${user.email}</td>
       </tr>
-      `).join("")}
-    `);
+      `
+        )
+        .join("")}
+    `
+    );
   })
   .get("/goal/:email", async (context) => {
     const email = context.params.email;
     if (email) {
-      const found = await kv.get(["goal",email]);
+      const found = await kv.get(["goal", email]);
       if (found.value) {
         context.response.body = found.value;
       } else {
@@ -172,9 +207,9 @@ router
   .delete("/goal/:email", async (context) => {
     const email = context.params.email;
     if (email) {
-      const found = await kv.get(["goal",email]);
+      const found = await kv.get(["goal", email]);
       if (found.value) {
-        await kv.delete(["goal",email]);
+        await kv.delete(["goal", email]);
         context.response.body = "user deleted";
       } else {
         context.response.body = "user not found";
@@ -190,14 +225,58 @@ router
       name: string;
       institution: string;
       location: string;
-    }>({prefix:["goal"]});
+    }>({ prefix: ["goal"] });
     const users = [];
-    for await (const {key, value} of list) {
-      users.push({email: key[1], name: value.name, institution: value.institution, location: value.location});
-    context.response.body = users;
+    for await (const { key, value } of list) {
+      users.push({
+        email: key[1],
+        name: value.name,
+        institution: value.institution,
+        location: value.location,
+      });
+      context.response.body = users;
     }
   })
-
+  .post("/goal/uploadJSON", async (context) => {
+    const body = context.request.body();
+    if (body.type === "json") {
+      const value: {
+        name: string;
+        institution: string;
+        location: string;
+        email: string;
+      }[] = await body.value;
+      if (value) {
+        for (const user of value) {
+          if (user.name && user.institution && user.location && user.email) {
+            // email validation
+            const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+            if (!emailRegex.test(user.email)) {
+              context.response.body = "Invalid email address";
+              context.response.status = 400;
+              return;
+            }
+            const found = await kv.get(["goal", user.email]);
+            if (!found.value) {
+              await kv.set(["goal", user.email], {
+                name: user.name,
+                institution: user.institution,
+                location: user.location,
+              });
+            } else {
+              context.response.body = "user already exists";
+              context.response.status = 400;
+            }
+          }
+        }
+        context.response.body = "users added.";
+      }
+    } else {
+      context.response.body =
+        "Invalid request. data must be json. Make sure to set Content-Type header to application/json";
+      context.response.status = 400;
+    }
+  });
 
 const app = new Application();
 app.use(oakCors()); // Enable CORS for All Routes
